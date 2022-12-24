46e874fd0d9e3f0c864100aa0ca0ba41b1707f36
James Patton Wilson <98632712+tahkmahnelle@users.noreply.github.com>
2022-12-24 03:12:53 -0500

    Add files via upload

---
+++ xABCD.js
@@ -0,0 +1,18 @@
+console.log("Toy Magik Inc. Productions");
+
+console.log("EHD9 SLPOE IDAMedia and Other Constitutives")
+
+console.log("Xabcd");
+
+// Assign values to x and a and b and c and d
+
+let x = 24;
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+
+// Add a + b + c + d and assign y the sum value
+let y = a + b + c + d;
+
+console.log(y);
---
+++ yplus1.js
@@ -0,0 +1,35 @@
+console.log("aadd.abheaa");
+console.log("25+1=26");
+console.log("y+1=z");
+
+console.log("ehd9pattonwilson_y+1=26");
+
+// Assign values to x and y
+
+let x = 1;
+let y = 25;
+
+// Add y + x and assign z the sum value
+
+let z = y + x;
+
+console.log(z);
+
+
+// Subtract z - y and assign w the difference 
+
+let w = y - z;
+
+console.log(w);
+
+// Subtract k - p and assign r the difference 
+
+let k = 0.012345679;
+let p = 0.12345679;
+
+let r = k - p;
+
+console.log(r);
+
+
+
